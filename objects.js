a8cbc2faf901459b2cb1bccf7424cd47db71d946
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-07 22:14:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function removeFromPlaylist(playlist, artistName){
 };
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist."Slowdive";
+  delete playlist.Slowdive
   return playlist;
 }
