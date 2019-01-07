e415c6bd00d94e7752932a73201d185bfc63d476
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-07 22:05:24 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = new Object({ artist: "song"});
+
+function updatePlaylist(object, key, value){
+ object[key] = value
+ return object
+}
+
+function removeFromPlaylist(playlist, artistName){
+  artist: "My Bloody Valentine", "Phil Ochs", "Slowdive"
+};
+
+delete playlist.artistName;
